5c3c666ff2a3b4af51bfd919c6146324dec83926
huongttt12 <huongttt12@topica.edu.vn>
2020-02-17 15:38:08 +0700

    spring boot security: simple authenticating user example

--- java8_api/src/main/java/vn/edumall/java8/apis/controllers/HelloController.java
+++ java8_api/src/main/java/vn/edumall/java8/apis/controllers/HelloController.java
@@ -5,8 +5,4 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
-    @RequestMapping("/")
-    public String index(){
-        return "Greeting from java-8 spring boot!";
-    }
 }
